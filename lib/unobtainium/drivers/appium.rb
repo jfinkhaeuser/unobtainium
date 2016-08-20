2f4f7a61be14f7b2e575ecd47061d6a00146fd9f
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-08-20 17:22:27 +0200

    Fixes #17

--- lib/unobtainium/drivers/appium.rb
+++ lib/unobtainium/drivers/appium.rb
@@ -78,7 +78,7 @@ module Unobtainium
           # the :url key if the normalized label is remote, and setting
           # appropriate options.
           set_url = options['appium_lib.server_url']
-          if set_url and not set_url == options['url']
+          if set_url and options['url'] and not set_url == options['url']
             warn "You have the remote URL '#{set_url}' set in your options, "\
               "so we're not replacing it with '#{options['url']}'!"
           else
