dd3caea7b1c9af098ce32e947f2ce6057e2049f8
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-11-08 12:37:34 +0100

    Adjust to code climate changes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,3 @@
-# Only start CodeClimate from travis
-if ENV['CODECLIMATE_REPO_TOKEN']
-  require 'codeclimate-test-reporter'
-  CodeClimate::TestReporter.start
-end
-
 # Always start SimpleCov
 require 'simplecov'
 SimpleCov.start do
