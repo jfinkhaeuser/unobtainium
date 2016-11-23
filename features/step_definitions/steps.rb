b0e7c9cbcf50e4fc3527941b12b688c39c02c479
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-11-23 16:21:09 +0100

    Add integration testing steps for driver identity

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -7,6 +7,29 @@
 # All rights reserved.
 #
 
+def store_ids
+  @driver_ids ||= []
+  @driver_ids << driver.object_id
+
+  @driver_impl_ids ||= []
+  @driver_impl_ids << driver.impl.object_id
+end
+
 Given(/^I navigate to the best website in the world$/) do
   driver.navigate.to "http://finkhaeuser.de"
+  store_ids
+end
+
+When(/^I navigate to the best website in the world again$/) do
+  driver.navigate.to "http://finkhaeuser.de"
+  store_ids
+end
+
+Then(/^I expect the driver in each case to be the same$/) do
+  if not @driver_ids[0] == @driver_ids[1]
+    raise "Driver instance changed!"
+  end
+  if not @driver_impl_ids[0] == @driver_impl_ids[1]
+    raise "Driver implementation instance changed!"
+  end
 end
--- features/world.feature
+++ features/world.feature
@@ -2,3 +2,8 @@ Feature: World
 
   Scenario: Driver and configuration loading
     Given I navigate to the best website in the world
+
+  Scenario: Driver re-use
+    Given I navigate to the best website in the world
+    When I navigate to the best website in the world again
+    Then I expect the driver in each case to be the same
