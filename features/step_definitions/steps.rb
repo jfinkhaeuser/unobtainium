7f5f11b49495ede203188012c911a0fbe61af8ba
Jens Finkhaeuser <jens@finkhaeuser.de>
2016-11-23 16:29:42 +0100

    Add integration scenario for multiple driver instances

--- features/step_definitions/steps.rb
+++ features/step_definitions/steps.rb
@@ -7,12 +7,18 @@
 # All rights reserved.
 #
 
-def store_ids
+def store_ids(the_driver = driver)
   @driver_ids ||= []
-  @driver_ids << driver.object_id
+  @driver_ids << the_driver.object_id
 
   @driver_impl_ids ||= []
-  @driver_impl_ids << driver.impl.object_id
+  @driver_impl_ids << the_driver.impl.object_id
+end
+
+
+Given(/^I have no driver IDs stored$/) do
+  @driver_ids = []
+  @driver_impl_ids = []
 end
 
 Given(/^I navigate to the best website in the world$/) do
@@ -31,5 +37,20 @@ Then(/^I expect the driver in each case to be the same$/) do
   end
   if not @driver_impl_ids[0] == @driver_impl_ids[1]
     raise "Driver implementation instance changed!"
+  end
+end
+
+When(/^I navigate to the best website in the world with another driver instance$/) do
+  new_driver = driver(:headless, desired_capabilities: { something: :new })
+  new_driver.navigate.to "http://finkhaeuser.de"
+  store_ids(new_driver)
+end
+
+Then(/^I expect to have two driver instances$/) do
+  if @driver_ids[0] == @driver_ids[1]
+    raise "Driver instance the same!"
+  end
+  if @driver_impl_ids[0] == @driver_impl_ids[1]
+    raise "Driver implementation instance the same!"
   end
 end
--- features/world.feature
+++ features/world.feature
@@ -1,5 +1,8 @@
 Feature: World
 
+  Background:
+    Given I have no driver IDs stored
+
   Scenario: Driver and configuration loading
     Given I navigate to the best website in the world
 
@@ -7,3 +10,8 @@ Feature: World
     Given I navigate to the best website in the world
     When I navigate to the best website in the world again
     Then I expect the driver in each case to be the same
+
+  Scenario: Multiple drivers
+    Given I navigate to the best website in the world
+    When I navigate to the best website in the world with another driver instance
+    Then I expect to have two driver instances
