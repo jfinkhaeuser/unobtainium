25f23b4620c6349587eaf6c1694053c8eab187a7
Jens Finkhaeuser <jens@finkhaeuser.de>
2018-04-09 11:49:16 +0200

    Bump own version

--- lib/unobtainium/version.rb
+++ lib/unobtainium/version.rb
@@ -3,10 +3,10 @@
 # unobtainium
 # https://github.com/jfinkhaeuser/unobtainium
 #
-# Copyright (c) 2016-2017 Jens Finkhaeuser and other unobtainium contributors.
+# Copyright (c) 2016-2018 Jens Finkhaeuser and other unobtainium contributors.
 # All rights reserved.
 #
 module Unobtainium
   # The current release version
-  VERSION = "0.12.0".freeze
+  VERSION = "0.13.0".freeze
 end
